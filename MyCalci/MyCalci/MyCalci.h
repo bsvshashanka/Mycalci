8a2eba98b8b2492e70209c73de0c704e1564a579
bsvshashanka <bsvshashanka03@gmail.com>
2024-01-28 15:15:14 +0530

    initial commit

---
+++ MyCalci/MyCalci/MyCalci.h
@@ -0,0 +1,18 @@
+//
+//  MyCalci.h
+//  MyCalci
+//
+//  Created by Shashanka on 28/01/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyCalci.
+FOUNDATION_EXPORT double MyCalciVersionNumber;
+
+//! Project version string for MyCalci.
+FOUNDATION_EXPORT const unsigned char MyCalciVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyCalci/PublicHeader.h>
+
+
---
+++ MyCalci/MyLogic.swift
@@ -0,0 +1,15 @@
+//
+//  ShankyLogic.swift
+//  ShankyCalculator
+//
+//  Created by Shashanka on 28/01/24.
+//
+
+import Foundation
+public class Calculator{
+    public init(){}
+    public static func add(x: Int, y: Int)->Int{return x+y}
+    public static func subtract(x: Int, y:Int)->Int{return x-y}
+    public static func multiply(x: Int, y: Int)->Int{return x*y}
+    public static func divide(x: Int, y: Int)->Int{return x/y}
+}
